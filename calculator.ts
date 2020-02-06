c2206311faec022f2947cee5b3173f915da093de
kevin-proctor <kevin.proctor26@gmail.com>
2020-02-06 16:23:56 -0600

    created variables and template for functions

---
+++ calculator.ts
@@ -0,0 +1,23 @@
+let multiply: any;
+let divide: any;
+let add: any;
+let subtract: any;
+let i: number = 0;
+let array_input[i]: number;
+
+
+// calculator starts with no display
+
+// press on/off button, on display 0 off display none
+
+// on number button press display  and store into array1
+
+// on math function button press close number array
+
+// on next number press clear display, display current button press, and store in array2
+
+// on CE button press clear last entered number
+
+// on C button press restart calculator
+
+// on = button press clear display then display solution
